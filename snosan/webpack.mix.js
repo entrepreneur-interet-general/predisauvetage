741601aae3618536637aab222d92864d5339242c
Antoine Augusti <antoine.augusti@developpement-durable.gouv.fr>
2018-03-29 14:28:55 +0200

    Increase top border size

--- snosan/webpack.mix.js
+++ snosan/webpack.mix.js
@@ -2,6 +2,10 @@ let mix = require('laravel-mix');
 
 mix
   .js('resources/assets/js/app.js', 'public/js')
-  .sass('resources/assets/sass/app.scss', 'public/css')
-  .version()
-  .sourceMaps();
+  .sass('resources/assets/sass/app.scss', 'public/css');
+
+if (mix.inProduction()) {
+    mix
+    .version()
+    .sourceMaps();
+}
