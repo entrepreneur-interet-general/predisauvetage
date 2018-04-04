2af7a310014fb82b7a598bdea7b42bc36d06a1cc
Antoine Augusti <AntoineAugusti@users.noreply.github.com>
2018-04-04 16:38:21 +0200

    Add basic registration and login tests #11
    * Add basic registration and login tests
    
    * Apply fixes from StyleCI (#12)

---
+++ snosan/tests/Feature/RegisterTest.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\User;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Tests\TestCase;
+
+class RegisterTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    /**
+     * The registration form can be displayed.
+     *
+     * @return void
+     */
+    public function testRegisterFormDisplayed()
+    {
+        $response = $this->get('/register');
+
+        $response->assertStatus(200);
+    }
+
+    /**
+     * A valid user can be registered.
+     *
+     * @return void
+     */
+    public function testRegistersAValidUser()
+    {
+        $user = factory(User::class)->make();
+
+        $response = $this->post('register', [
+            'name'                  => $user->name,
+            'email'                 => $user->email,
+            'password'              => 'secret',
+            'password_confirmation' => 'secret',
+        ]);
+
+        $response->assertStatus(302);
+
+        $this->assertAuthenticated();
+    }
+
+    /**
+     * An invalid user is not registered.
+     *
+     * @return void
+     */
+    public function testDoesNotRegisterAnInvalidUser()
+    {
+        $user = factory(User::class)->make();
+
+        $response = $this->post('register', [
+            'name'                  => $user->name,
+            'email'                 => $user->email,
+            'password'              => 'secret',
+            'password_confirmation' => 'invalid',
+        ]);
+
+        $response->assertSessionHasErrors('password');
+
+        $this->assertGuest();
+    }
+}
