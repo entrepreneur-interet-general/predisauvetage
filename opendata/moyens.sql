224ec5a6f0d7a40e9e2fdd593f66b285add94e83
Antoine Augusti <antoine.augusti@developpement-durable.gouv.fr>
2018-04-09 17:09:48 +0200

    Add SQL request to generate moyens table

---
+++ opendata/moyens.sql
@@ -0,0 +1,14 @@
+select
+  smm.OPERATION_ID operation_id,
+  smm.NO_ORDRE numero_ordre,
+  scm.LIBELLE moyen,
+  sccm.LIBELLE categorie_moyen,
+  scda.LIBELLE domaine_action,
+  scam.LIBELLE autorite_moyen,
+  smm.DATE_HEURE_DEPART date_heure_debut,
+  smm.DATE_HEURE_DEPART + smm.duree date_heure_fin
+from SECMAR.SEC_MOYEN_MEO smm
+join SECMAR.SEC_C_MOYEN scm on smm.MOYEN_ID = scm.MOYEN_ID
+join SECMAR.SEC_C_CAT_MOYEN sccm on scm.CAT_MOYEN_ID = sccm.CAT_MOYEN_ID
+join SECMAR.SEC_C_DOMAINE_ACTION scda on scm.DOMAINE_ACTION_ID = scda.DOMAINE_ACTION_ID
+join SECMAR.SEC_C_AUTORITE_MOYEN scam on smm.AUTORITE_MOYEN_ID = scam.AUTORITE_MOYEN_ID
