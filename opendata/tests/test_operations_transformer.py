2297f173b84754a55b3c5f4b5aac9c4bf92d2529
Antoine Augusti <antoine.augusti@developpement-durable.gouv.fr>
2018-04-23 10:59:04 +0200

    Add OperationsTransformer

---
+++ opendata/tests/test_operations_transformer.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+import unittest
+import os.path as op
+import os
+
+from transformers.operations import OperationsTransformer
+
+
+def filepath(f):
+    return op.join(op.abspath(op.join(__file__, op.pardir, op.pardir)), f)
+
+
+class TestOperationsTransformer(unittest.TestCase):
+    def test_simple_config(self):
+        in_file = filepath('tests/files/operations.csv')
+        expected_file = filepath('tests/files/expected_operations.csv')
+
+        self.run_for_files(in_file, expected_file)
+
+    def run_for_files(self, in_file, expected_file):
+        out_file = '/tmp/out.csv'
+
+        OperationsTransformer(in_file).transform(out_file)
+
+        self.assertEquals(
+            open(out_file, 'r').readlines(),
+            open(expected_file, 'r').readlines(),
+        )
+
+        # os.remove(out_file)
---
+++ opendata/transformers/operations.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+
+import pandas as pd
+import numpy as np
+
+
+class OperationsTransformer(object):
+    def __init__(self, filepath):
+        super(OperationsTransformer, self).__init__()
+        self.filepath = filepath
+
+    def transform(self, output):
+        df = pd.read_csv(
+            self.filepath,
+            delimiter=',',
+            parse_dates=['date_heure_reception_alerte', 'date_heure_fin_operation'],
+            true_values=['Y'],
+            false_values=['N']
+        )
+        df['cross_sitrep'] = df.apply(lambda r: self.cross_sitrep(r), axis=1)
+        df['fuseau_horaire'] = self.fuseau_horaire(df.cross)
+        df['pourquoi_alerte'] = df.pourquoi_alerte.replace({r'^(SAR|MAS|DIV|SUR).*' : r'\1'}, regex=True)
+        df.to_csv(output, index=False)
+
+    def cross_sitrep(self, row):
+        if pd.isna(row['numero_sitrep']):
+            return np.nan
+        return '{cross} {year}/{sitrep_nb}'.format(
+            cross=row['cross'],
+            year=row['date_heure_reception_alerte'].year,
+            sitrep_nb=int(row['numero_sitrep'])
+        )
+
+    def fuseau_horaire(self, series):
+        return series.map({
+            'Adge': 'Europe/Paris',
+            'Antilles-Guyane': 'America/Cayenne',
+            'Corse': 'Europe/Paris',
+            'Corsen': 'Europe/Paris',
+            'Gris-Nez': 'Europe/Paris',
+            'Guadeloupe': 'America/Guadeloupe',
+            'Guyane': 'America/Guyana',
+            'Jobourg': 'Europe/Paris',
+            'La Garde': 'Europe/Paris',
+            'La Réunion': 'Indian/Reunion',
+            'Martinique': 'America/Martinique',
+            'Mayotte': 'Indian/Mayotte',
+            'Nouvelle-Calédonie': 'Pacific/Tahiti',
+            'Polynésie': 'Pacific/Noumea',
+            'Soulac': 'Europe/Paris',
+            'Étel': 'Europe/Paris',
+        })
