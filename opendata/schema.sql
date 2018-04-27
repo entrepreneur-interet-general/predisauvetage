133b81075cbcd461c347d5fad2bfca4a9f4b2a31
Antoine Augusti <antoine.augusti@developpement-durable.gouv.fr>
2018-04-27 15:57:23 +0200

    Improve SQL queries

--- opendata/schema.sql
+++ opendata/schema.sql
@@ -1,5 +1,6 @@
+DROP TABLE IF EXISTS public.operations CASCADE;
 CREATE TABLE public.operations (
-    "operation_id" int PRIMARY KEY,
+    "operation_id" bigint PRIMARY KEY,
     "pourquoi_alerte" varchar(50),
     "moyen_alerte" varchar(100),
     "qui_alerte" varchar(100),
@@ -17,22 +18,23 @@ CREATE TABLE public.operations (
     "vent_direction" smallint,
     "vent_force" smallint,
     "mer_force" smallint,
-    "date_heure_reception_alerte" timestamp with time zone,
-    "date_heure_fin_operation" timestamp with time zone,
+    "date_heure_reception_alerte" timestamp without time zone,
+    "date_heure_fin_operation" timestamp without time zone,
     "numero_sitrep" smallint,
     "cross_sitrep" varchar(50),
     "fuseau_horaire" varchar(25)
 );
 
 CREATE INDEX ON operations(pourquoi_alerte);
-CREATE INDEX ON operations(cross);
+CREATE INDEX ON operations("cross");
 CREATE INDEX ON operations(departement);
 CREATE INDEX ON operations((date_heure_reception_alerte::date));
 CREATE INDEX ON operations((date_heure_fin_operation::date));
 
 
+DROP TABLE IF EXISTS public.flotteurs;
 CREATE TABLE public.flotteurs (
-    "operation_id" int references operations,
+    "operation_id" bigint references operations on delete cascade,
     "numero_ordre" smallint,
     "pavillon" varchar(50),
     "resultat_flotteur" varchar(50),
@@ -63,15 +65,16 @@ CREATE INDEX ON flotteurs(type_flotteur);
 CREATE INDEX ON flotteurs(categorie_flotteur);
 
 
+DROP TABLE IF EXISTS public.moyens;
 CREATE TABLE public.moyens (
-    "operation_id" int references operations,
+    "operation_id" bigint references operations on delete cascade,
     "numero_ordre" smallint,
     "moyen" varchar(100),
     "categorie_moyen" varchar(100),
     "domaine_action" varchar(50),
     "autorite_moyen" varchar(100),
-    "date_heure_debut" timestamp with time zone,
-    "date_heure_fin" timestamp with time zone
+    "date_heure_debut" timestamp without time zone,
+    "date_heure_fin" timestamp without time zone
 );
 
 CREATE INDEX ON moyens(operation_id);
@@ -81,8 +84,9 @@ CREATE INDEX ON moyens(domaine_action);
 CREATE INDEX ON moyens(autorite_moyen);
 
 
+DROP TABLE IF EXISTS public.resultats_humain;
 CREATE TABLE public.resultats_humain (
-    "operation_id" int references operations,
+    "operation_id" bigint references operations on delete cascade,
     "categorie_personne" varchar(50),
     "resultat_humain" varchar(50),
     "nombre" smallint,
--- opendata/sql/moyens.sql
+++ opendata/sql/moyens.sql
@@ -8,6 +8,7 @@ select
   smm.DATE_HEURE_DEPART date_heure_debut,
   smm.DATE_HEURE_DEPART + smm.duree date_heure_fin
 from SECMAR.SEC_MOYEN_MEO smm
+join SECMAR.SEC_OPERATION so on so.OPERATION_ID = smm.OPERATION_ID and so.ETAT_FICHE_ID = 'VALIDE'
 join SECMAR.SEC_C_MOYEN scm on smm.MOYEN_ID = scm.MOYEN_ID
 join SECMAR.SEC_C_CAT_MOYEN sccm on scm.CAT_MOYEN_ID = sccm.CAT_MOYEN_ID
 join SECMAR.SEC_C_DOMAINE_ACTION scda on scm.DOMAINE_ACTION_ID = scda.DOMAINE_ACTION_ID
