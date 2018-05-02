fc20ac203bc3da00f4e693147ecd8e488c73564f
Antoine Augusti <antoine.augusti@developpement-durable.gouv.fr>
2018-05-02 15:07:05 +0200

    Route to serve documentation

---
+++ snosan/app/Http/Controllers/CircleController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use GuzzleHttp\Client as GuzzleClient;
+use Cache;
+
+
+class CircleController extends Controller
+{
+
+    public function latestDocumentationHtml($format)
+    {
+        return Cache::remember("doc-$format", 10, function () use($format) {
+            $body = $this->get('tree/master');
+            $buildNumber = collect(json_decode($body, true))->first()['build_num'];
+            $body = $this->get("$buildNumber/artifacts");
+            $data = collect(json_decode($body, true));
+
+            $url = $data->filter(function($e) use($format) {
+                return $e['pretty_path'] == "schema.$format";
+            })->first()['url'];
+
+            return (new GuzzleClient())->get($url)->getBody()->getContents();
+        });
+    }
+
+    private function get($url)
+    {
+        $client = new GuzzleClient();
+        $base = 'https://circleci.com/api/v1.1/project/github/entrepreneur-interet-general/predisauvetage/';
+        $response = $client->get($base.$url, ['headers' => ['Accept' => 'application/json']]);
+        return $response->getBody()->getContents();
+    }
+}
--- snosan/routes/web.php
+++ snosan/routes/web.php
@@ -2,5 +2,6 @@
 
 Route::get('/', 'HomeController@index')->name('home');
 Route::get('/dashboard', 'DashboardController@index')->name('dashboard.index');
+Route::get('/docs/{format}', 'CircleController@latestDocumentationHtml')->where('format', '(html|md)');
 
 Auth::routes();
