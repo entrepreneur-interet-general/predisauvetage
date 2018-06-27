04b36b24e1fe427fe42e0ca1c750ee603af31b7b
Antoine Augusti <antoine.augusti@gmail.com>
2018-06-27 10:11:39 +0200

    Update tests

--- opendata/tests/opendata/test_operations_stats.py
+++ opendata/tests/opendata/test_operations_stats.py
@@ -7,7 +7,15 @@ class TestOperationsStatsTransformer(BaseTest):
     def test_columns(self):
         self.assertEquals(
             OperationsStatsTransformer('/tmp/fake').columns_to_drop(),
-            ['concerne_snosan']
+            [
+              "concerne_snosan",
+              "nombre_moyens_nautiques_engages",
+              "nombre_moyens_terrestres_engages",
+              "nombre_moyens_aeriens_engages",
+              "duree_engagement_moyens_nautiques_minutes",
+              "duree_engagement_moyens_terrestres_minutes",
+              "duree_engagement_moyens_aeriens_minutes"
+            ]
         )
 
     def transformers(self):
