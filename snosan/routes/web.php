ed4b23c78d710af7982caaf252ca338ff123bba1
Antoine Augusti <antoine.augusti@developpement-durable.gouv.fr>
2018-08-20 14:34:26 +0200

    Remove Laravel files

--- snosan/routes/web.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-Route::get('/', 'HomeController@index')->name('home');
-Route::get('/dashboard', 'DashboardController@index')->name('dashboard.index');
-Route::get('/docs/{format}', 'CircleController@latestDocumentationHtml')->where('format', '(html|md)');
-
-Auth::routes();
