fd57b5b5b6f09825c48202b7a291e9b0a4324c70
Antoine Augusti <antoine.augusti@developpement-durable.gouv.fr>
2018-11-05 09:58:52 +0100

    Indentation

--- airflow/dags/extract_secmar.py
+++ airflow/dags/extract_secmar.py
@@ -350,19 +350,19 @@ queries = {
         where annee between 2000 and 2008
     ''',
     'unset_tide_data': '''
-    select count(1) = 0
-    from operations_stats stats
-    join operations o on o.operation_id = stats.operation_id
-    where stats.distance_cote_metres < 20000
-      and o."cross" not in ('Antilles-Guyane', 'Corse', 'Guadeloupe', 'Guyane', 'La Garde', 'La Réunion', 'Martinique', 'Mayotte', 'Nouvelle-Calédonie', 'Polynésie')
-      and stats.maree_coefficient is null
+        select count(1) = 0
+        from operations_stats stats
+        join operations o on o.operation_id = stats.operation_id
+        where stats.distance_cote_metres < 20000
+          and o."cross" not in ('Antilles-Guyane', 'Corse', 'Guadeloupe', 'Guyane', 'La Garde', 'La Réunion', 'Martinique', 'Mayotte', 'Nouvelle-Calédonie', 'Polynésie')
+          and stats.maree_coefficient is null
     ''',
     'unset_shore_distance': '''
-    select count(1) = 0
-    from operations_stats stats
-    join operations op on op.operation_id = stats.operation_id
-    where op.latitude is not null
-      and (stats.distance_cote_milles_nautiques is null or stats.distance_cote_metres is null)
+        select count(1) = 0
+        from operations_stats stats
+        join operations op on op.operation_id = stats.operation_id
+        where op.latitude is not null
+          and (stats.distance_cote_milles_nautiques is null or stats.distance_cote_metres is null)
     '''
 }
 
