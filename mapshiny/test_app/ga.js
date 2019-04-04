ac7f926f6af84319df610de075124de9aa07da57
Antoine Augusti <antoine.augusti@data.gouv.fr>
2019-04-04 15:11:13 +0200

    Don't include external script

--- mapshiny/test_app/ga.js
+++ mapshiny/test_app/ga.js
@@ -1,4 +1,3 @@
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-133494078-2"></script>
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
