de8910650e5cfeb68aaef6bfa4883d1947829701
Antoine Augusti <antoine.augusti@gmail.com>
2023-07-09 11:54:48 +0200

    Replace insert_moyens

--- opendata/sql/secmar_csv/insert_moyens.sql
+++ opendata/sql/secmar_csv/insert_moyens.sql
@@ -8,28 +8,26 @@ ALTER TABLE moyens ALTER COLUMN date_heure_fin DROP NOT NULL;
 ALTER TABLE moyens ALTER COLUMN duree_engagement_minutes DROP NOT NULL;
 
 INSERT INTO moyens (
-  "operation_id",
-  "numero_ordre",
-  "moyen",
-  "categorie_moyen",
-  "domaine_action",
-  "autorite_moyen",
-  "date_heure_debut",
-  "date_heure_fin",
-  "duree_engagement_minutes"
+  operation_id,
+  numero_ordre,
+  moyen,
+  categorie_moyen,
+  domaine_action,
+  autorite_moyen,
+  date_heure_debut,
+  date_heure_fin,
+  duree_engagement_minutes
 )
-SELECT
-  secmar_operation_id operation_id,
-  null numero_ordre,
-  "SEC_MOYEN_MEO_moyen_id" moyen,
-  "SEC_C_MOYEN_cat_moyen_id" categorie_moyen,
-  t.domaine_action,
-  "SEC_MOYEN_MEO_autorite_moyen_id" autorite_moyen,
-  "SEC_MOYEN_MEO_date_heure_depart" date_heure_debut,
-  "SEC_MOYEN_MEO_duree" date_heure_fin,
-  extract(epoch from "SEC_MOYEN_MEO_duree" - "SEC_MOYEN_MEO_date_heure_depart")/60 duree_engagement_minutes
-FROM secmar_csv_moyen m
-LEFT JOIN (
-  SELECT DISTINCT moyen, domaine_action FROM moyens
-) t on t.moyen = m."SEC_MOYEN_MEO_moyen_id"
-WHERE secmar_operation_id IN (SELECT operation_id FROM operations);
+select
+  sco.secmar_operation_id,
+  sjm.numero_ordre,
+  sjm.moyen,
+  sjm.categorie_moyen,
+  sjm.domaine_action,
+  sjm.autorite_moyen,
+  sjm.date_heure_debut,
+  sjm.date_heure_fin,
+  sjm.duree_engagement_minutes
+from snosan_json_moyens sjm
+join secmar_csv_operation sco on sco.operation_long_name = replace(sjm.chrono, '-', '_')
+where sco.secmar_operation_id not in (select distinct operation_id from moyens) and sco.secmar_operation_id IN (SELECT operation_id FROM operations);
