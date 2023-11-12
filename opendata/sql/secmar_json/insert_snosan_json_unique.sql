f204f0d2a3797bb9a49ae493c041e2d9bfbcefd9
Antoine Augusti <antoine.augusti@gmail.com>
2023-11-12 11:09:43 +0100

    Improve insert_snosan_json_unique

--- opendata/sql/secmar_json/insert_snosan_json_unique.sql
+++ opendata/sql/secmar_json/insert_snosan_json_unique.sql
@@ -2,12 +2,6 @@ truncate snosan_json_unique;
 
 insert into snosan_json_unique (data)
 select distinct
-  first_value(data) over(partition by data->>'id' order by s.id desc) as data
-from snosan_json s
-join (
-  select
-    data->>'id' id,
-    max(data->>'version') max_version
-  from snosan_json
-  group by 1
-) t on s.data->>'id' = t.id and s.data->>'version' = t.max_version;
+  first_value(data) over(partition by data->>'id' order by id desc) as data
+from snosan_json
+;
