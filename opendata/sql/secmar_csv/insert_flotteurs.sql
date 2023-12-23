46e2e044a28fa557fde5e4107cda10780f10b58b
Antoine Augusti <antoine.augusti@gmail.com>
2023-12-23 15:19:56 +0100

    Use snosan_json_flotteurs for flotteurs

--- opendata/sql/secmar_csv/insert_flotteurs.sql
+++ opendata/sql/secmar_csv/insert_flotteurs.sql
@@ -5,26 +5,19 @@ ALTER TABLE flotteurs ALTER COLUMN categorie_flotteur DROP NOT NULL;
 
 INSERT INTO flotteurs (
   "operation_id",
-  "numero_ordre",
   "pavillon",
   "resultat_flotteur",
   "type_flotteur",
   "categorie_flotteur",
-  "numero_immatriculation",
-  "immatriculation_omi"
+  "longueur"
 )
 SELECT
-  secmar_operation_id operation_id,
-  null numero_ordre,
-  "SEC_FLOTTEUR_IMPLIQUE_pavillon_id" pavillon,
-  "SEC_FLOTTEUR_IMPLIQUE_resultat_flotteur_id" resultat_flotteur,
-  "SEC_FLOTTEUR_IMPLIQUE_type_flotteur_id" type_flotteur,
-  t.categorie_flotteur categorie_flotteur,
-  "SEC_FLOTTEUR_IMPLIQUE_num_immat_fr" numero_immatriculation,
-  "SEC_FLOTTEUR_IMPLIQUE_num_imo" immatriculation_omi
-  -- "SEC_FLOTTEUR_IMPLIQUE_nom",
-FROM secmar_csv_flotteur f
-LEFT JOIN (
-  SELECT DISTINCT type_flotteur, categorie_flotteur FROM flotteurs
-) t ON t.type_flotteur = f."SEC_FLOTTEUR_IMPLIQUE_type_flotteur_id"
-WHERE secmar_operation_id IN (SELECT operation_id FROM operations);
+  sco.secmar_operation_id operation_id,
+  sjf.pavillon "pavillon",
+  sjf.resultat_flotteur "resultat_flotteur",
+  sjf.type_flotteur "type_flotteur",
+  sjf.categorie_flotteur "categorie_flotteur",
+  sjf.longueur longueur
+FROM snosan_json_flotteurs sjf
+JOIN secmar_csv_operation sco on sco.operation_long_name = replace(sjf.chrono, '-', '_')
+WHERE sco.secmar_operation_id not in (select distinct operation_id from flotteurs) and sco.secmar_operation_id IN (SELECT operation_id FROM operations);
