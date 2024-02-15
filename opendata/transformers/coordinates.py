41d0502e4d29d48fa486de1919986cd57688c7a7
Antoine Augusti <antoine.augusti@gmail.com>
2024-02-15 14:14:11 +0100

    Amélioration du parsing des coordonnées

--- opendata/transformers/coordinates.py
+++ opendata/transformers/coordinates.py
@@ -3,14 +3,14 @@ import re
 
 # Example: 47°03.29'N - 002°14.59'W
 PATTERN_DM = re.compile(
-    r"(?P<lat>\d+)°(?P<lat_m>\d+.\d+)'(?P<lat_dir>N|S) - (?P<lon>\d+)°(?P<lon_m>\d+.\d+)'(?P<lon_dir>W|E)"
+    r"(?P<lat>\d+)°(?P<lat_m>\d+.\d+)'(?P<lat_dir>N|S) (-|/) (?P<lon>\d+)°(?P<lon_m>\d+.\d+)'(?P<lon_dir>W|E)"
 )
 # Example: 43°32'27"N - 003°58'41"E
 PATTERN_DMS = re.compile(
-    r"(?P<lat>\d+)°(?P<lat_m>\d+)'(?P<lat_s>\d+)\"?(?P<lat_dir>N|S) - (?P<lon>\d+)°(?P<lon_m>\d+)'(?P<lon_s>\d+)\"?(?P<lon_dir>W|E)"
+    r"(?P<lat>\d+)°(?P<lat_m>\d+)'(?P<lat_s>\d+)\"?(?P<lat_dir>N|S) (-|/) (?P<lon>\d+)°(?P<lon_m>\d+)'(?P<lon_s>\d+)\"?(?P<lon_dir>W|E)"
 )
 # Example: -51,033333 - 2,0515
-PATTERN_DD = re.compile(r"(?P<lat>-?\d+(,|.)\d+) - (?P<lon>-?\d+(,|.)\d+)")
+PATTERN_DD = re.compile(r"(?P<lat>-?\d+(,|.)\d+) (-|/) (?P<lon>-?\d+(,|.)\d+)")
 
 
 def parse(content):
