650e141798f4dd62071a62d7964d0123610ba399
Antoine Augusti <antoine.augusti@gmail.com>
2025-09-13 17:40:05 +0200

    Gris-Nez est métropolitain

--- opendata/transformers/operations.py
+++ opendata/transformers/operations.py
@@ -55,8 +55,8 @@ class OperationsTransformer(BaseTransformer):
 
         if row["cross"] in cross_hors_metropole:
             return False
-        if row["est_metropolitain"] is None:
-            return row["cross"] not in cross_hors_metropole and row["cross"] != "Gris-Nez"
+        if row["est_metropolitain"] in [np.nan, None]:
+            return row["cross"] not in cross_hors_metropole
         return row["est_metropolitain"]
 
     def vent_direction_categorie(self, series):
