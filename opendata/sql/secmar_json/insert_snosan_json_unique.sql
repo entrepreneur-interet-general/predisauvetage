5030c044481aa9981307498c030f45d59034c613
Antoine Augusti <antoine.augusti@gmail.com>
2026-03-25 15:10:37 +0100

    Use chrono for insert_snosan_json_unique

--- opendata/sql/secmar_json/insert_snosan_json_unique.sql
+++ opendata/sql/secmar_json/insert_snosan_json_unique.sql
@@ -2,6 +2,6 @@ truncate snosan_json_unique;
 
 insert into snosan_json_unique (data)
 select distinct
-  first_value(data) over(partition by data->>'id' order by id desc) as data
+  first_value(data) over(partition by data->>'chrono' order by id desc) as data
 from snosan_json
 ;
